366e36eb138bd471fa333fee7e5a210b21b83e08
Kevin Strange <kevin.strange1000@gmail.com>
2023-06-27 15:23:55 +0100

    components updated

--- spin-city-records/src/components/Layout/Footer.tsx
+++ spin-city-records/src/components/Layout/Footer.tsx
@@ -1,9 +1,35 @@
+import Image from "next/image";
+import logo from "../../pages/Home/images/logo.png";
+
 export default function Footer() {
   return (
     <>
-      <div className="mt-8 h-28 w-screen border-t border-[#333333] bg-black">
-        <h1 className="text-white">Footer</h1>
-      </div>
+      <footer>
+        <div className="flex justify-evenly bg-purple-950 text-white">
+          <div className="flex items-center">
+            <Image src={logo} alt="logo" height={150} />
+          </div>
+          <ul className="m-10">
+            <li>Shop With Us</li>
+            <li>Sell With Us</li>
+            <li>About Us</li>
+            <li>Follow Us</li>
+          </ul>
+          <ul className="m-10">
+            <li>Search</li>
+            <li>Browse Collection</li>
+            <li>My Account</li>
+            <li>My Orders</li>
+            <li>View Basket</li>
+          </ul>
+          <ul className="m-10">
+            <li>FAQ</li>
+            <li>Help</li>
+            <li>Contact Us</li>
+            <li>Legal Stuff</li>
+          </ul>
+        </div>
+      </footer>
     </>
   );
 }
