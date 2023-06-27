010aed4051ab947c3501082b0d2f9f566ba2f14c
angusmellor <theangusmellor@gmail.com>
2023-06-27 16:02:20 +0100

    index resolved

--- spin-city-records/src/pages/index.tsx
+++ spin-city-records/src/pages/index.tsx
@@ -1,62 +1,8 @@
-// import { SignInButton } from "@clerk/nextjs";
-// import { useUser } from "@clerk/clerk-react";
-// import Head from "next/head";
-// import Link from "next/link";
-// import ManageUser from "~/components/userButton";
-// import { RouterOutputs, api } from "~/utils/api";
-// import SignUpStep from "~/components/addUser";
-
-// const CreateListing = () => {
-//   const { user } = useUser();
-//   if (!user) return null;
-//   return (
-//     <div>
-//       <button className="border border-slate-800">
-//         <Link href="/create-listing">Create Listing</Link>
-//       </button>
-//     </div>
-//   );
-// };
-
-// //we gonna need this later - for now we just want to see if we can get the data
-// type ListingWithUser = RouterOutputs["listings"]["getAll"][number];
-
-// const ListingView = (props: ListingWithUser) => {
-//   const { listing, user } = props;
-//   return (
-//     <div key={listing.id}>
-//       @{user.username}---
-//       {listing.description} {listing.price} {listing.currency}
-//     </div>
-//   );
-// };
-
-// export default function Home() {
-//   const user = useUser();
-
-//   return (
-//     <>
-//       {!user.isSignedIn && <SignInButton />}
-//       {user.isSignedIn && (
-//         <div>
-//           <ManageUser />
-//           <CreateListing/>
-//         </div>
-//       )}
-//       {/* <div>
-//         {[...data]?.map((fullListing) => (
-//           <ListingView {...fullListing} key={fullListing.listing.id} />
-//         ))}
-//       </div> */}
-//     </>
-//   );
-// }
 
 // import { useUser, SignInButton, UserButton } from "@clerk/nextjs"
 
 // export default function Home() {
 //   const user = useUser();
-<<<<<<< HEAD
 
 //   return (
 //     <>
@@ -66,17 +12,6 @@
 //   );
 // }
         
-=======
-
-//   return (
-//     <>
-//       {!user.isSignedIn && <SignInButton />}
-//       {user.isSignedIn && <UserButton afterSignOutUrl="/"/>}
-//     </>
-//   );
-// }
-
->>>>>>> 6af8aa77a293717d3038aa4087316ef8075b079e
 import Link from "next/link";
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
