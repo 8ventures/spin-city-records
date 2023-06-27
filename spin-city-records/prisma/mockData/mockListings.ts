7ca1a88d629271c6564cc098f8790a63a3ef9617
angusmellor <theangusmellor@gmail.com>
2023-06-27 17:05:43 +0100

    db fully seeded

--- spin-city-records/prisma/mockData/mockListings.ts
+++ spin-city-records/prisma/mockData/mockListings.ts
@@ -1,28 +1,31 @@
 export const mockListings = [
     {
-      userId: "user_2RmYnqMfglB4HTLO9IUfhERAJWK",
+      userId: "cljeh2erp000buasgz53yue8g",
+      albumId: 'cljeh2c3k0004uasgpnlhofu7',
       price: 25.99,
       currency: "USD",
       condition: "Near Mint",
       weight: "standard",
       format: "12''",
       speed: "33RPM",
-      special: ["colored", "limited edition"],
+      special: [{id:39}, {id: 40}],
       description: "Limited edition colored vinyl in near mint condition. Comes with original sleeve."
     },
     {
-      userId: "user_2RmYnqMfglB4HTLO9IUfhERAJWK",
+      userId: "cljeh2erp000buasgz53yue8g",
+      albumId: 'cljeh2c3k0004uasgpnlhofu7',
       price: 15.99,
       currency: "USD",
       condition: "Very Good Plus",
       weight: "standard",
       format: "12''",
       speed: "33RPM",
-      special: ["box set"],
+      special: [{id: 38}],
       description: "Complete box set in very good plus condition. Includes bonus booklet and poster."
     },
     {
-      userId: "user_2RmYnqMfglB4HTLO9IUfhERAJWK",
+      userId: "cljeh2f8e000cuasgf24hfunm",
+      albumId: 'cljeh2c3k0004uasgpnlhofu7',
       price: 9.99,
       currency: "USD",
       condition: "Good",
@@ -33,18 +36,20 @@ export const mockListings = [
       description: "Used vinyl in good condition. Minor surface scratches, plays without skips."
     },
     {
-      userId: "user_2RmYnqMfglB4HTLO9IUfhERAJWK",
+      userId: "cljeh2f8e000cuasgf24hfunm",
+      albumId: 'cljeh2cnw0006uasgdkpacdjh',
       price: 29.99,
       currency: "USD",
       condition: "Mint",
       weight: "standard",
       format: "12''",
       speed: "33RPM",
-      special: ["signed"],
+      special: [{id: 37}],
       description: "Brand new, mint condition vinyl. Autographed by the artist."
     },
     {
-      userId: "user_2RmZv2FV2FCGOpRQ9majPLkd9Bc",
+      userId: "cljeh2fix000duasgzwi70fht",
+      albumId: 'cljeh2cnw0006uasgdkpacdjh',
       price: 12.99,
       currency: "USD",
       condition: "Very Good",
@@ -55,18 +60,20 @@ export const mockListings = [
       description: "Used vinyl in very good condition. Sleeve shows some signs of wear."
     },
     {
-      userId: "user_2RmZv2FV2FCGOpRQ9majPLkd9Bc",
+      userId: "cljeh2fix000duasgzwi70fht",
+      albumId: 'cljeh2d1m0008uasgmogs6ky4',
       price: 18.99,
       currency: "USD",
       condition: "Good Plus",
       weight: "standard",
       format: "12''",
       speed: "33RPM",
-      special: ["pictured"],
+      special: [{id: 36}],
       description: "Vinyl in good plus condition. Picture disc edition with original sleeve."
     },
     {
-      userId: "user_2RmZv2FV2FCGOpRQ9majPLkd9Bc",
+      userId: "cljeh2fix000duasgzwi70fht",
+      albumId: 'cljeh2d1m0008uasgmogs6ky4',
       price: 6.99,
       currency: "USD",
       condition: "Very Good Plus",
@@ -77,25 +84,27 @@ export const mockListings = [
       description: "Used vinyl in very good plus condition. Well-preserved sleeve with minimal wear."
     },
     {
-      userId: "user_2RmZv2FV2FCGOpRQ9majPLkd9Bc",
+      userId: "cljeh2erp000buasgz53yue8g",
+      albumId: 'cljeh2dyk000auasg9ds7goth',
       price: 14.99,
       currency: "USD",
       condition: "Very Good",
       weight: "standard",
       format: "12''",
       speed: "33RPM",
-      special: ["test pressing"],
+      special: [{id: 42}],
       description: "Test pressing vinyl in very good condition. Plain white sleeve."
     },
     {
-      userId: "user_2RehOIuW9oVpa8uq4QE6rVuTIXQ",
+      userId: "cljeh2erp000buasgz53yue8g",
+      albumId: 'cljeh2dyk000auasg9ds7goth',
       price: 22.99,
       currency: "USD",
       condition: "Near Mint",
       weight: "standard",
       format: "12''",
       speed: "33RPM",
-      special: ["remastered"],
+      special: [{id: 43}],
       description: "Remastered edition vinyl in near mint condition. Original sleeve included."
     }
   ];
--- spin-city-records/prisma/seed.ts
+++ spin-city-records/prisma/seed.ts
@@ -3,62 +3,71 @@ import { mockArtists } from './mockData/mockArtists';
 import { mockAlbums } from './mockData/mockAlbums';
 import { mockUsers } from './mockData/mockUsers'
 import { mockListings } from './mockData/mockListings';
+import { editions } from './mockData/editions'
 
 const prisma = new PrismaClient();
 
 async function main() {
   try {
     console.log('Removing existing data');
-    await prisma.album.deleteMany();
-    await prisma.artist.deleteMany();
+    // await prisma.album.deleteMany();
+    // await prisma.artist.deleteMany();
+    await prisma.listing.deleteMany();
+    // await prisma.special.deleteMany(); // commented out to stop indexs of special types changing
+    // await prisma.user.deleteMany();
     console.log('Data Removed')
 
-    for (const a of mockArtists) {
-      await prisma.artist.create({
-        data: a
-      })
-    }
-    console.log('Artist added')   
+    // for (const a of mockArtists) {
+    //   await prisma.artist.create({
+    //     data: a
+    //   })
+    // }
+    // console.log('Artist added')   
 
-    for (const a of mockAlbums) {
-      const { artist, ...album} = a
-      const dbArtist = await prisma.artist.findFirst({
-        where: {
-          name: artist
-        }
-      })
-      if (dbArtist) {
-        await prisma.album.create({
-          data: {
-            ...album,
-            artistId: dbArtist.id
+    // for (const a of mockAlbums) {
+    //   const { artist, ...album} = a
+    //   const dbArtist = await prisma.artist.findFirst({
+    //     where: {
+    //       name: artist
+    //     }
+    //   })
+    //   if (dbArtist) {
+    //     await prisma.album.create({
+    //       data: {
+    //         ...album,
+    //         artistId: dbArtist.id
+    //       }
+    //     })
+    //   }
+    // }
+    // console.log('Albums added')
+    
+    // for (const e of editions) {
+    //   await prisma.special.create({
+    //     data: e
+    //   })
+    // }
+    // console.log('editions added')
+
+    // for (const u of mockUsers) {
+    //   await prisma.user.create({
+    //     data: u
+    //   })
+    // }
+    // console.log('Users Added')
+
+    for (const l of mockListings) {
+      const {special, ...listing} = l
+      await prisma.listing.create({
+        data: {
+          ...listing,
+          special: {
+            connect: special
           }
-        })
-      }
-    }
-    console.log('Albums added')
-
-    for (const u of mockUsers) {
-      await prisma.user.create({
-        data: u
+        }
       })
     }
-    console.log('Users Added')
-
-    const dbAlbums = await prisma.album.findMany();
-    if (dbAlbums.length > 0 ) {
-      for (const l of mockListings) {
-        const index = Math.floor(Math.random()*(dbAlbums.length))
-        if (dbAlbums[index]) {
-          await prisma.listing.create({
-            data: {
-              ...l,
-              albumId: dbAlbums[index]!.id
-            }
-          })
-        }
-      }
-    }
+    console.log('Listings added')
   } catch (e) {
     console.log(e)
   }
