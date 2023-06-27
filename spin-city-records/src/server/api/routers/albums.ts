d94f1cc63313381a5174c982d31a4e021f3d0da3
angusmellor <theangusmellor@gmail.com>
2023-06-27 17:20:32 +0100

    listings and albums route

--- spin-city-records/src/server/api/routers/albums.ts
+++ spin-city-records/src/server/api/routers/albums.ts
@@ -1,12 +1,27 @@
 import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from 'zod';
 
 export const albumsRouter = createTRPCRouter({
-  getAll: publicProcedure.query( ({ctx}) => {
+  getAll: publicProcedure.query( async ({ctx}) => {
     try {
-      const albums = ctx.prisma.album.findMany();
+      const albums = await ctx.prisma.album.findMany();
       return albums
     } catch (e) {
       console.log(e)
     }
+  }),
+  getById: publicProcedure
+  .input(z.string())
+  .query( async ({ctx, input}) => {
+    try {
+      const album = await ctx.prisma.album.findUnique({
+        where: {
+          id: input
+        }
+      })
+      return album
+    } catch (e) {
+      console.log(e)
+    } 
   })
 });
