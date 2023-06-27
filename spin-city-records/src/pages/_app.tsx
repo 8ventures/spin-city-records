949f6a5e3f03810f7daa0150b3c5d2fa23465151
Luis Carrasco <luis@8ventures.io>
2023-06-27 16:52:55 +0200

    feat: after merge

--- spin-city-records/src/pages/_app.tsx
+++ spin-city-records/src/pages/_app.tsx
@@ -4,15 +4,13 @@ import "~/styles/globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import AlbumPage from "./album/[id]";
 
-
 const MyApp: AppType = ({ Component, pageProps }) => {
   return (
-
     <ClerkProvider>
-      <Component {...pageProps} />;
+      {/* <Component {...pageProps} />; */}
+      <AlbumPage></AlbumPage>
     </ClerkProvider>
-  )
-
+  );
 };
 
 export default api.withTRPC(MyApp);
