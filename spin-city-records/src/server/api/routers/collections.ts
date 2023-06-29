582bd25da1cfb25f0aec2f14ecfa2806e2f985f0
Luis Carrasco <luis@8ventures.io>
2023-06-29 10:36:27 +0200

    feat: collections router done

---
+++ spin-city-records/src/server/api/routers/collections.ts
@@ -0,0 +1,34 @@
+import { createTRPCRouter, publicProcedure } from "~/server/api/trpc";
+import { z } from "zod";
+
+export const collectionRouter = createTRPCRouter({
+  getAll: publicProcedure.query(async ({ ctx }) => {
+    try {
+      const collections = await ctx.prisma.collection.findMany();
+      return collections;
+    } catch (e) {
+      console.log(e);
+    }
+  }),
+  getById: publicProcedure
+    .input(
+      z.object({
+        id: z.string(),
+      })
+    )
+    .query(async ({ ctx, input }) => {
+      const { id } = input;
+      try {
+        const collection = await ctx.prisma.collection.findUnique({
+          where: { id },
+          include: {
+            albums: true,
+          },
+        });
+        return collection;
+      } catch (e) {
+        console.log("here");
+        console.log(e);
+      }
+    }),
+});
