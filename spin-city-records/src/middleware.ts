9d3174f0ef0bb286de141e21f67997e7f265d04a
angusmellor <theangusmellor@gmail.com>
2023-06-30 11:20:43 +0100

    middleware fix

--- spin-city-records/src/middleware.ts
+++ spin-city-records/src/middleware.ts
@@ -1,6 +1,6 @@
 import { authMiddleware } from "@clerk/nextjs";
 export default authMiddleware({
-  publicRoutes: ["/", "/api/trpc/collections.getById", "/api/trpc/albums.getAll", "/api/trpc/collections.getById,albums.getAll", "/sign-in"]
+  publicRoutes: ["/", "/api/trpc/collections.getById", "/api/trpc/albums.getAll", "/api/trpc/collections.getById,albums.getAll"]
 });
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
