7198ec7cce90d5953ab66b97e3f0aa3f036d05de
angusmellor <theangusmellor@gmail.com>
2023-06-30 23:31:03 +0100

    form styling

--- spin-city-records/src/components/Profile/onboardingForm.tsx
+++ spin-city-records/src/components/Profile/onboardingForm.tsx
@@ -1,11 +1,50 @@
-
-
+import { useRouter } from "next/router";
+import { api } from "~/utils/api";
+import Image, { type StaticImageData } from "next/image"
+import circle from '../../../public/circle.svg'
+import stripe from '../../../public/stripe-ar21.svg'
+import { Separator } from "@radix-ui/react-separator";
 
 export default function OnboardingForm() {
+    const { mutate: createSeller, data: url, isSuccess }= api.sellers.create.useMutation();
+    const router = useRouter()
+  
+    const handleClick = () => {
+      createSeller()
+      if (isSuccess && url) {
+        console.log('here')
+        router.push(url).catch((e) => console.log(e))
+      } else {
+        console.log('Unable to register')
+      }
+    }
+  
+    return (
+      <div className="flex flex-col items-center">
+        <div className=' text-white w-96 h-fit text-2xl text-center my-2'>
+          We use Stripe to make sure you get paid on time and to keep your personal bank and details secure.
+          <br/><br/>Click <strong>Accept and continue</strong> to set up your payments on Stripe.
+        </div>
+        <div className="flex space-between h-32 items-center my-2">
+          <Image
+          src={circle as StaticImageData}
+          alt="circle"
+          className=" h-24 w-24 mr-5"
+          />
+          <Separator 
+            className=" bg-slate-400 data-[orientation=vertical]:h-full data-[orientation=vertical]:w-px mx-[15px]"
+            orientation="vertical"
+          />
+          <Image
+          src={stripe as StaticImageData}
+          alt="stripe logo"
+          className=" h-24 w-48"
+          />
+        </div>
 
-  return (
-    <>
-      <div>Become a seller</div>
-    </>
-  )
-}
+        <button className="text-white  w-fit h-fit text-2xl mt-5" onClick={handleClick}>
+          Accept and Continue
+        </button>
+      </div>
+    )
+  }
