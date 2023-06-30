6605ec91aef3802cf8e03aa5910847dd7f891a03
Erban Danaj <danajerban@gmail.com>
2023-06-30 10:42:55 +0200

    fix routing

--- spin-city-records/src/middleware.ts
+++ spin-city-records/src/middleware.ts
@@ -1,8 +1,8 @@
 import { authMiddleware } from "@clerk/nextjs";
-
 export default authMiddleware({
-  publicRoutes: ["/"]
+  publicRoutes: ["/", "/api/trpc/collections.getById", "/api/trpc/albums.getAll", "/api/trpc/collections.getById,albums.getAll"]
 });
+
 export const config = {
   matcher: ["/((?!.*\\..*|_next).*)", "/", "/(api|trpc)(.*)"],
 };
