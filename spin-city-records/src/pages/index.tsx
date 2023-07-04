bb8860d87a20ab368aac4b286218916326197352
Luis Carrasco <luis@8ventures.io>
2023-07-04 22:59:10 +0200

    smaller images

--- spin-city-records/src/pages/index.tsx
+++ spin-city-records/src/pages/index.tsx
@@ -45,30 +45,31 @@ export default function Home() {
   return (
     <Layout>
       <SplideCarousel />
-      <section className="mx-auto flex max-w-full flex-col items-center justify-center overflow-hidden">
+      <section className="mx-auto flex w-full flex-col items-center justify-center overflow-hidden">
         <h1
-          className={`mt-4 w-full text-center text-2xl text-[#FF5500] sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl ${serif.className}`}
+          className={`mt-4 w-full text-center text-2xl text-white sm:text-2xl md:text-2xl lg:text-3xl xl:text-3xl ${serif.className}`}
         >
           SHOP MUSIC
         </h1>
-        <div className="my-2 w-5/6 border-b border-[#A1A1A1]" />
+        <div className="my-8 w-5/6 border-b border-[#A1A1A1]" />
         <MusicSection
           title={"RECENTLY ADDED"}
           collection={recentlyAdded}
           loading={recentlyAddedQueryIsLoading}
         />
-        <div className="my-2 w-5/6 border-b border-[#A1A1A1]" />
+        <div className="my-8 w-5/6 border-b border-[#A1A1A1]" />
         <MusicSection
           title={"NEW RELEASES"}
           collection={recentlyAdded}
           loading={recentlyAddedQueryIsLoading}
         />
-        <div className="my-2 w-5/6 border-b border-[#A1A1A1]" />
+        <div className="my-8 w-5/6 border-b border-[#A1A1A1]" />
         <MusicSection
           title={"BEST SELLERS"}
           collection={recentlyAdded}
           loading={recentlyAddedQueryIsLoading}
         />
+        <div className="my-4" />
       </section>
     </Layout>
   );
