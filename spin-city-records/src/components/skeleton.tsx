ac9319ca48458d287866cd24c47195dd4e7ac65b
angusmellor <theangusmellor@gmail.com>
2023-07-05 21:54:43 +0100

    skeleton filled

--- spin-city-records/src/components/skeleton.tsx
+++ spin-city-records/src/components/skeleton.tsx
@@ -8,7 +8,7 @@ export default function Skeleton({className}: SpinnerProps) {
 
   return (
     <div role="status">
-      <div className={cn("h-full w-full animate-pulse rounded-xl border border-[#333333]", className,)}></div>
+      <div className={cn("h-full w-full animate-pulse rounded-xl bg-zinc-800", className,)}></div>
     </div>
   )
 }
