0fcf602401c825163c12976dc0de92f59d8a2f92
Erban Danaj <danajerban@gmail.com>
2023-07-06 13:37:53 +0200

    -1

--- spin-city-records/src/components/Album/SortComponent.tsx
+++ spin-city-records/src/components/Album/SortComponent.tsx
@@ -23,31 +23,31 @@ export default function SortBy({
 
   return (
     <DropdownMenu.Root>
-      <DropdownMenu.Trigger className="text-md my-4 mr-12 inline text-white outline-none sm:mr-14">
+      <DropdownMenu.Trigger className="text-md my-4 mr-12 inline text-white outline-none sm:mr-14 font-bold">
         {triggerName}{" "}
         <ChevronDownIcon className="inline-block h-5 w-5 text-white" />
       </DropdownMenu.Trigger>
 
-      <DropdownMenu.Content className="text-md w-40 rounded-xl bg-white p-4 text-black">
-        <DropdownMenu.Label className="text-md font-semibold">
+      <DropdownMenu.Content className="text-md w-40 rounded-2xl bg-black p-4 text-white">
+        <DropdownMenu.Label className="text-md text-center font-black">
           Price
         </DropdownMenu.Label>
         <DropdownMenu.Item
-          className="text-md cursor-pointer text-left outline-none"
+          className="text-md cursor-pointer text-center rounded-xl outline-none py-1 hover:bg-gray-900"
           onClick={() => handleSortOption("lowToHigh")}
         >
           {`Low to High`}
         </DropdownMenu.Item>
         <DropdownMenu.Item
-          className="text-md cursor-pointer text-left outline-none"
+          className="text-md cursor-pointer text-center rounded-xl outline-none py-1 hover:bg-gray-900"
           onClick={() => handleSortOption("highToLow")}
         >
           {`High to Low`}
         </DropdownMenu.Item>
-        <DropdownMenu.Separator className="my-4 border-b" />
+        <DropdownMenu.Separator className="" />
 
         <DropdownMenu.Item
-          className="text-md cursor-pointer text-center font-semibold  outline-none"
+          className="text-md cursor-pointer border-t border-b border-gray-900 text-center rounded-xl  font-semibold my-2 py-1 text-custom-orange outline-none hover:bg-gray-900"
           onClick={clearSort}
         >
           Clear Sort
